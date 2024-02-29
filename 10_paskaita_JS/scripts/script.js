e7db45d8ed98ab2d8daeecc8f374a2821c6901ab
jarmolovskis <kazimiras.jarmolovskis@gmail.com>
2024-02-29 16:36:09 +0200

    Užduočių sprendimai

---
+++ 10_paskaita_JS/scripts/script.js
@@ -0,0 +1,8 @@
+// Išvedam tekstą į konsolę
+console.log('Sveiki! Aš iš script.js failo.');
+
+// Išvedam tekstą iššokusiame langelyje
+// alert('Kažkoks tekstas!');
+
+// Išvedame tekstą html body srityje (kaip turinį)
+document.write('Tekstas dokumente');
